eb714c7d205df7555f2de43729f1afe5766a729d
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-08-07 20:02:19 +0100

    feat (Header) : add header component

---
+++ ballamas_ecommerce/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+ import Banner from './Banner'
+import NavBar from './NavBar'
+
+const Header = () => {
+  return (
+    <div>
+          <Banner />
+          <NavBar/>
+    </div>
+  )
+}
+
+export default Header
