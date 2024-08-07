7566960dc86f995ca2097b2654c4de6a7e95f9fb
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-08-07 19:48:47 +0100

    feat (banner) : add banner section

---
+++ ballamas_ecommerce/src/components/header/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+      <h1>Sign up and get 20% off for all new arrivals collections</h1>
+    </div>
+  )
+}
+
+export default Banner
