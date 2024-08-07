279009345fda1fd2d689c2eb4ddb7beea17a7642
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-08-07 17:54:19 +0100

    initial commit

---
+++ ballamas_ecommerce/src/App.jsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+ 
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+     
+         
+      </div>
+     
+    </>
+  )
+}
+
+export default App
