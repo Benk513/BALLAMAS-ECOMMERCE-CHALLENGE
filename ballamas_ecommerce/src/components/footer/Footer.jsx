306f6e98f5486607f6161d2509bdbb7d8706d34b
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-08-08 12:50:26 +0100

    feat(Footer) : add footer section

---
+++ ballamas_ecommerce/src/components/footer/Footer.jsx
@@ -0,0 +1,36 @@
+import FooterMenu from "./FooterMenu"
+
+ 
+const Footer = () => {
+  return (
+    <footer className=" bg-b-black  py-14 mt-20">
+
+      <div className="container flex justify-between ">
+        <div className="flex-col gap-3">
+          <h2 className="logo-white text-4xl font-chillax">BALLAMAS</h2>
+          <p className="text-b-gray mt-4 w-4/5">Subscribe to our newsletter  for upcoming products and best discount for all items</p>
+
+
+          <div className="flex gap-2 mt-3">
+            
+            <input type="text" name="" id="" placeholder="Your email" className="rounded-full px-4 py-2 bg-b-black border border-b-white text-b-white font-display w-[316px] focus:ring-2" />
+
+            <button className="btn-secondary font-display">Subscribe</button>
+          </div>
+
+        </div>
+
+        <FooterMenu/>
+
+
+
+
+      </div>
+      <div className="container mt-6"><p className="text-b-dark-gray text-center">© BALLAMAS 2024 by waris</p></div>
+    </footer>
+      
+    
+  )
+}
+
+export default Footer
---
+++ ballamas_ecommerce/src/components/footer/FooterMenu.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const FooterMenu = () => {
+  return (
+   
+    <div className="flex gap-10">
+
+    <div className="text-b-white">
+      <h2>Product</h2>
+      <ul>
+        <li>Jacket</li>
+        <li>T-shirt</li>
+        <li>Jacket</li>
+        <li>Shoe</li>
+        <li>Sunglasses</li>
+      
+      </ul>
+    </div>
+
+    <div className="text-b-white">
+      <h2>Categories</h2>
+      <ul>
+        <li>Man</li>
+        <li>Woman</li>
+        <li>Kids</li>
+        <li>Gift</li>
+        <li>New Arrival</li>
+      
+      </ul>
+    </div>
+
+    <div className="text-b-white">
+      <h2>Our Social Media</h2>
+      <ul>
+        <li>Instagram</li>
+        <li>Facebook</li>
+        <li>Youtube</li>
+        <li>X</li>
+      </ul>
+    </div>
+
+
+
+  </div>
+  )
+}
+
+export default FooterMenu
