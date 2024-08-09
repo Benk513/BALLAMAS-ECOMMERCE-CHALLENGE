143384bef8d758eefb2bd2b1da1e7203fa7c5fce
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-08-09 21:15:05 +0100

    feat (Discount) : add a 20% discount for items over 100 cad

--- ballamas_ecommerce/src/pages/home/Product.jsx
+++ ballamas_ecommerce/src/pages/home/Product.jsx
@@ -1,21 +1,50 @@
  
-const Product = () => {
+const Product = ({ item }) => {
+
+  //object destructuring
+  const urlImage = item.node.featuredImage.url
+  const title = item.node.title
+  let amount = (item.node.variants.edges[0].node.price.amount) *1
+  
+  console.log(amount)
   return (
     <div className="text-start relative">
-          <img src="/summerShirt.jpg" alt="product image" className="rounded-[32px]" />
-          <h2 className="font-display mt-4 text-3xl">SUMMER SHIRT </h2>
-          <p className="text-b-dark-gray font-display">$99</p>
+      <a href="#" className="group relative mb-2 block h-100 overflow-hidden rounded-lg bg-gray-100 lg:mb-3 ">
+          <img src={ urlImage} alt="product image" className="rounded-[32px] object-cover transition duration-300 group-hover:scale-110 h-full w-full" loading="lazy" /></a>
+          <h2 className="font-display mt-4 text-xl lg:text-2xl">{title} </h2>
+      <p className="text-b-dark-gray text-xl lg:text-xl font-display">CAD {amount}</p>
 
 
-
-          <div className="absolute top-5 left-5 ">
-              <p className="bg-b-white rounded-full px-4 py-2 text-base font-display font-bold">
+ 
+{/* get 20% reduction for every product over 100 cad */}
+      {amount > 100 ?
+        <div className="absolute top-5 left-5 ">
+              <p className="bg-b-white rounded-full px-2 py-2  lg:px-4 lg:py-2 text-xs lg:text-base font-display font-bold">
               
               Get OFF 20%
           </p>
-          </div>
+      </div> :''}
+  
+        
+      
     </div>
   )
 }
 
 export default Product
+
+
+
+{/* <div>
+<a href="#" className="group relative mb-2 block h-80 overflow-hidden rounded-lg bg-gray-100 lg:mb-3">
+  <img src="https://images.unsplash.com/photo-1619066045029-5c7e8537bd8c?auto=format&q=75&fit=crop&w=600" loading="lazy"  className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+</a>
+
+<div>
+  <a href="#" className="hover:gray-800 mb-1 text-gray-500 transition duration-100 lg:text-lg">Lazy Bottle</a>
+
+  <div className="flex items-end gap-2">
+    <span className="font-bold text-gray-800 lg:text-lg">$9.00</span>
+  </div>
+</div>
+</div> */}
--- ballamas_ecommerce/src/pages/home/Products.jsx
+++ ballamas_ecommerce/src/pages/home/Products.jsx
@@ -1,7 +1,47 @@
 import FilterButtons from './FilterButtons'
+import {useState , useEffect} from 'react'
 import Product from './Product'
 
+const BASE_URL = ''
+
 const Products = () => {
+
+  const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+   
+  
+  useEffect(() => {
+  
+
+    const fetchProducts = async () => {
+
+ 
+      setIsLoading(true)
+
+      const request = await fetch('https://mock.shop/api?query={products(first:%2020){edges%20{node%20{id%20title%20description%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}')
+      const response = await request.json();
+      console.log(response.data.products.edges);
+      setProducts(response.data.products.edges);
+
+      setIsLoading(false)
+        
+      
+    }
+    fetchProducts()
+  }, [])
+  
+
+  
+
+  if (isLoading) {
+    return (
+      <div className='bg-orange-500 h-screen w-full'>Loading...</div>
+    )
+    
+  }
+
+
+
   return (
       <div className='text-center font-chillax text-3xl mt-[72px]'>
           <h1 className='text-2xl lg:text-4xl'>Discover the latest trends in summer fashion. Shop now and refresh your wardrobe with our stylish summer shirts.</h1>
@@ -9,14 +49,18 @@ const Products = () => {
           
           <FilterButtons />
           
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6'
-          >
-           <Product/>
-           <Product/>
-           <Product/>
-           <Product/>
-           <Product/>
-           <Product/>
+    
+      
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6'>
+        
+
+        
+         {products.map((item,index) => {
+          
+          return  <Product key={index} item={item} />
+        })} 
+         
+       
               
           </div>
 
